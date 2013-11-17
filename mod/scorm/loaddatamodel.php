b5ecf5ddb04ad6e3e2f629f8730cc23529de37a9
Dan Marsden <dan@danmarsden.com>
2013-11-18 12:34:50 +1300

    MDL-35422 don't use datamodel.php with TCAPI

--- mod/scorm/loaddatamodel.php
+++ mod/scorm/loaddatamodel.php
@@ -102,12 +102,7 @@ if (scorm_version_check($scorm->version, SCORM_13)) {
 header('Content-Type: text/javascript; charset=UTF-8');
 
 $scorm->version = strtolower(clean_param($scorm->version, PARAM_SAFEDIR));   // Just to be safe.
-/*
- * TCAPI - Bypass loading of API since we don't need it when is TCAPI.
- */
-if (scorm_version_check($scorm->version, SCORM_TCAPI)) {
-	// Do nothing and bypass API loading
-} else if (file_exists($CFG->dirroot.'/mod/scorm/datamodels/'.$scorm->version.'.js.php')) {
+if (file_exists($CFG->dirroot.'/mod/scorm/datamodels/'.$scorm->version.'.js.php')) {
     include_once($CFG->dirroot.'/mod/scorm/datamodels/'.$scorm->version.'.js.php');
 } else {
     include_once($CFG->dirroot.'/mod/scorm/datamodels/scorm_12.js.php');
