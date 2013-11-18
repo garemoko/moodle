c22a6bbe64f4411897dc022abd16d695a1219fa3
Dan Marsden <dan@danmarsden.com>
2013-11-18 13:37:43 +1300

    MDL-35433 SCORM: tidy up tin can files

--- mod/scorm/datamodels/tincanlib.php
+++ mod/scorm/datamodels/tincanlib.php
@@ -22,46 +22,46 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-function scorm_get_tincan_launch_params($scorm,$sco,$launchUrl) {
-	global $CFG,$USER;
-	require_once $CFG->dirroot.'/local/tcapi/locallib.php';
-	// verify tcapi plugin exists, if not, return nothing
-	if (!file_exists($CFG->dirroot.'/local/tcapi/locallib.php'))
-		return '';
-	// Call the TCAPI local webservice locallib for token and endpoint.
-	$token = local_tcapi_get_user_token();
-	// Generate activity_id as unique using URI method (also provides LRS with metadata path)
-	$activity_id = str_ireplace($sco->launch, 'tincan.xml', $launchUrl);
-	// Determine connector for launch params.
-	$connector = (stripos($sco->launch, '?') !== false) ? '&' : '?';
-	// build a registration (not used for anything at this time)
-	$registration = md5($USER->id.$sco->id);
-	// add version (not used for anything at this time)
-	$revision = $scorm->revision;
-	// gather all the launch params
-	$launch_params = array(
-	'endpoint'=>TCAPI_ENDPOINT,
-	'auth'=>$token->token,
-	'moodle_mod'=>'scorm',
-	'moodle_mod_id'=>$sco->id,
-	'revision'=>$revision,
-	'registration'=>$registration,
-	'activity_id'=>$activity_id,
-	);
-	// Webservice content address for content endpoint (Articulate Mobile Player)
-	// Only provide if using the pluginfile.php method for delivery.
-	if ($pos = strpos($launchUrl, '/pluginfile.php')) {
-		$wscontenturl = substr($launchUrl, $pos, strlen($sco->launch)*-1);
-		$wscontenturl = str_ireplace('/pluginfile.php', '/', $wscontenturl);
-		$launch_params['content_endpoint'] = TCAPI_CONTENT_ENDPOINT.$wscontenturl;
-		$launch_params['content_token'] = $token->token;
-	}
-	$params_encoded = array();
-	foreach ($launch_params as $lk=>$lv)
-		array_push($params_encoded, $lk.'='.rawurlencode($lv));
-	
-	// Build and return content launch string
-	return $connector.implode("&",$params_encoded);
+function scorm_get_tincan_launch_params($scorm, $sco, $launchurl) {
+    global $CFG, $USER;
+    require_once($CFG->dirroot.'/local/tcapi/locallib.php');
+    // verify tcapi plugin exists, if not, return nothing.
+    if (!file_exists($CFG->dirroot.'/local/tcapi/locallib.php')) {
+        return '';
+    }
+    // Call the TCAPI local webservice locallib for token and endpoint.
+    $token = local_tcapi_get_user_token();
+    // Generate activity_id as unique using URI method (also provides LRS with metadata path).
+    $activityid = str_ireplace($sco->launch, 'tincan.xml', $launchurl);
+    // Determine connector for launch params.
+    $connector = (stripos($sco->launch, '?') !== false) ? '&' : '?';
+    // build a registration (not used for anything at this time).
+    $registration = md5($USER->id.$sco->id);
+    // add version (not used for anything at this time).
+    $revision = $scorm->revision;
+    // gather all the launch params.
+    $launchparams = array(
+    'endpoint' => TCAPI_ENDPOINT,
+    'auth' => $token->token,
+    'moodle_mod' => 'scorm',
+    'moodle_mod_id' => $sco->id,
+    'revision' => $revision,
+    'registration' => $registration,
+    'activity_id' => $activityid);
+    // Webservice content address for content endpoint (Articulate Mobile Player)
+    // Only provide if using the pluginfile.php method for delivery.
+    if ($pos = strpos($launchurl, '/pluginfile.php')) {
+        $wscontenturl = substr($launchurl, $pos, strlen($sco->launch) * -1);
+        $wscontenturl = str_ireplace('/pluginfile.php', '/', $wscontenturl);
+        $launchparams['content_endpoint'] = TCAPI_CONTENT_ENDPOINT.$wscontenturl;
+        $launchparams['content_token'] = $token->token;
+    }
+    $paramsencoded = array();
+    foreach ($launchparams as $lk => $lv) {
+        array_push($paramsencoded, $lk.'='.rawurlencode($lv));
+    }
+    // Build and return content launch string.
+    return $connector.implode("&", $paramsencoded);
 }
 
 function scorm_get_tincan_manifest($blocks, $scoes) {
@@ -71,7 +71,7 @@ function scorm_get_tincan_manifest($blocks, $scoes) {
 
     static $manifest;
     static $organization;
-    static $course_id;
+    static $courseid;
 
     if (count($blocks) > 0) {
         foreach ($blocks as $block) {
@@ -98,7 +98,7 @@ function scorm_get_tincan_manifest($blocks, $scoes) {
 
                     array_pop($parents);
                 break;
-            	case 'ACTIVITIES':
+                case 'ACTIVITIES':
                     if (!isset($scoes->defaultorg) && isset($block['attrs']['DEFAULT'])) {
                         $scoes->defaultorg = $block['attrs']['DEFAULT'];
                     }
@@ -107,33 +107,33 @@ function scorm_get_tincan_manifest($blocks, $scoes) {
                     }
                 break;
                 case 'ACTIVITY':
-                	if (empty($course_id) && isset($block['attrs']['TYPE']) && $block['attrs']['TYPE'] == 'course') {
-                		
-	                    $org = $parent = array_pop($parents);
-	                    array_push($parents, $parent);
+                    if (empty($courseid) && isset($block['attrs']['TYPE']) && $block['attrs']['TYPE'] == 'course') {
 
-                		$course_id = $identifier = $block['attrs']['ID'];
-	                    $scoes->elements[$manifest][$organization][$identifier] = new stdClass();
-	                    $scoes->elements[$manifest][$organization][$identifier]->identifier = $identifier;
-	                    $scoes->elements[$manifest][$organization][$identifier]->parent = $parent->identifier;
-	                    $scoes->elements[$manifest][$organization][$identifier]->launch = '';
-	                    $scoes->elements[$manifest][$organization][$identifier]->scormtype = 'sco';
-	
-	                    $parent = new stdClass();
-	                    $parent->identifier = $identifier;
-	                    $parent->organization = $organization;
-	                    array_push($parents, $parent);
-	
-	                    if (!empty($block['children'])) {
-	                        $scoes = scorm_get_tincan_manifest($block['children'], $scoes);
-	                    }
-	                    // If a title was found for the course block, apply it to the organization block
-	                    if (isset($scoes->elements[$manifest][$organization][$identifier]->title)) {
-	                    	$scoes->elements[$manifest][$org->organization][$org->identifier]->title = $scoes->elements[$manifest][$organization][$identifier]->title;
-	                    }
-	
-	                    array_pop($parents);
-                	}
+                        $org = $parent = array_pop($parents);
+                        array_push($parents, $parent);
+
+                        $courseid = $identifier = $block['attrs']['ID'];
+                        $scoes->elements[$manifest][$organization][$identifier] = new stdClass();
+                        $scoes->elements[$manifest][$organization][$identifier]->identifier = $identifier;
+                        $scoes->elements[$manifest][$organization][$identifier]->parent = $parent->identifier;
+                        $scoes->elements[$manifest][$organization][$identifier]->launch = '';
+                        $scoes->elements[$manifest][$organization][$identifier]->scormtype = 'sco';
+
+                        $parent = new stdClass();
+                        $parent->identifier = $identifier;
+                        $parent->organization = $organization;
+                        array_push($parents, $parent);
+
+                        if (!empty($block['children'])) {
+                            $scoes = scorm_get_tincan_manifest($block['children'], $scoes);
+                        }
+                        // If a title was found for the course block, apply it to the organization block.
+                        if (isset($scoes->elements[$manifest][$organization][$identifier]->title)) {
+                            $scoes->elements[$manifest][$org->organization][$org->identifier]->title = $scoes->elements[$manifest][$organization][$identifier]->title;
+                        }
+
+                        array_pop($parents);
+                    }
                 break;
                 case 'NAME':
                     $parent = array_pop($parents);
@@ -200,13 +200,13 @@ function scorm_parse_tincan($scorm, $manifest) {
     $replacement = '&amp;';
     $xmltext = preg_replace($pattern, $replacement, $xmltext);
 
-    $objXML = new scormxml2array();
-    $manifests = $objXML->parse($xmltext);
+    $objxml = new scormxml2array();
+    $manifests = $objxml->parse($xmltext);
     $scoes = new stdClass();
     $scoes->version = 'TCAPI';
     $scoes = scorm_get_tincan_manifest($manifests, $scoes);
     if (count($scoes->elements) > 0) {
-        $olditems = $DB->get_records('scorm_scoes', array('scorm'=>$scorm->id));
+        $olditems = $DB->get_records('scorm_scoes', array('scorm' => $scorm->id));
         foreach ($scoes->elements as $manifest => $organizations) {
             foreach ($organizations as $organization => $items) {
                 foreach ($items as $identifier => $item) {
@@ -221,7 +221,7 @@ function scorm_parse_tincan($scorm, $manifest) {
                         }
                     }
 
-                    // Insert the new SCO, and retain the link between the old and new for later adjustment
+                    // Insert the new SCO, and retain the link between the old and new for later adjustment.
                     $id = $DB->insert_record('scorm_scoes', $newitem);
                     if (!empty($olditems) && ($olditemid = scorm_array_search('identifier', $newitem->identifier, $olditems))) {
                         $olditems[$olditemid]->newid = $id;
@@ -232,7 +232,7 @@ function scorm_parse_tincan($scorm, $manifest) {
                         $data->scoid = $id;
                         foreach ($optionaldatas as $optionaldata) {
                             if (isset($item->$optionaldata)) {
-                                $data->name =  $optionaldata;
+                                $data->name = $optionaldata;
                                 $data->value = $item->$optionaldata;
                                 $dataid = $DB->insert_record('scorm_scoes_data', $data);
                             }
@@ -247,15 +247,15 @@ function scorm_parse_tincan($scorm, $manifest) {
         }
         if (!empty($olditems)) {
             foreach ($olditems as $olditem) {
-                $DB->delete_records('scorm_scoes', array('id'=>$olditem->id));
-                $DB->delete_records('scorm_scoes_data', array('scoid'=>$olditem->id));
+                $DB->delete_records('scorm_scoes', array('id' => $olditem->id));
+                $DB->delete_records('scorm_scoes_data', array('scoid' => $olditem->id));
                 if (isset($olditem->newid)) {
                     $DB->set_field('scorm_scoes_track', 'scoid', $olditem->newid, array('scoid' => $olditem->id));
                 }
-                $DB->delete_records('scorm_scoes_track', array('scoid'=>$olditem->id));
+                $DB->delete_records('scorm_scoes_track', array('scoid' => $olditem->id));
             }
         }
-        $DB->set_field('scorm', 'version', $scoes->version, array('id'=>$scorm->id));
+        $DB->set_field('scorm', 'version', $scoes->version, array('id' => $scorm->id));
         $scorm->version = $scoes->version;
     }
 
@@ -271,17 +271,17 @@ function scorm_parse_tincan($scorm, $manifest) {
  */
 function scorm_tincan_generate_simple_sco($scorm) {
     global $DB;
-    // find the old one
-    $scos = $DB->get_records('scorm_scoes', array('scorm'=>$scorm->id));
+    // Find the old one.
+    $scos = $DB->get_records('scorm_scoes', array('scorm' => $scorm->id));
     if (!empty($scos)) {
         $sco = array_shift($scos);
     } else {
         $sco = new object();
     }
-    // get rid of old ones
-    foreach($scos as $oldsco) {
-        $DB->delete_records('scorm_scoes', array('id'=>$oldsco->id));
-        $DB->delete_records('scorm_scoes_track', array('scoid'=>$oldsco->id));
+    // Get rid of old ones.
+    foreach ($scos as $oldsco) {
+        $DB->delete_records('scorm_scoes', array('id' => $oldsco->id));
+        $DB->delete_records('scorm_scoes_track', array('scoid' => $oldsco->id));
     }
 
     $sco->identifier = 'TCAPI1';
